ac8d88edc9a863bb4c14aee245e3eab3107a7e3d
Michael Matinez <michael.martinez.dev@gmail.com>
2021-11-01 20:32:51 -0600

    fixing charfield

--- mysite/mysite/models/task.py
+++ mysite/mysite/models/task.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class task(models.Model):
-    id = models.CharField(unique=True,auto_created=True)
-    title = models.CharField(required=True)
-    description = models.CharField(required=True)
+    id = models.CharField("id",unique=True,auto_created=True)
+    title = models.CharField("title")
+    description = models.CharField("description")
     status = models.CharField(max_length=11,choices=TYPE_SELECT)
-    date = models.DateField(required=True)
+    date = models.DateField("date")
